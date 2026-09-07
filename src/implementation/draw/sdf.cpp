2f6842e29eb75e233bab4ab5c1b56d91025c2fa1
maf <marralesfios@gmail.com>
2026-09-07 13:34:45 +0800

    better text AA with fwidth

--- src/implementation/draw/sdf.cpp
+++ src/implementation/draw/sdf.cpp
@@ -2,7 +2,7 @@
 #include<sgl/draw/impl/buffer_creation.hpp>
 namespace sgl{
     using namespace std::literals;
-    SDFRenderer::SDFRenderer() : p(u8"#version 460 core\nlayout(location=0)in vec2 p;layout(location=1)in vec2 t;layout(location=0)out vec2 T;void main(){T=t;gl_Position=vec4(p,0.,1.);}"sv,u8"#version 460 core\nout vec4 C;layout(location=0)in vec2 t;layout(location=0)uniform sampler2D i;layout(location=1)uniform vec3 c;void main(){C=vec4(c,smoothstep(.495,.505,texture(i,t).r));}"sv){
+    SDFRenderer::SDFRenderer() : p(u8"#version 460 core\nlayout(location=0)in vec2 p;layout(location=1)in vec2 t;layout(location=0)out vec2 T;void main(){T=t;gl_Position=vec4(p,0.,1.);}"sv,u8"#version 460 core\nout vec4 C;layout(location=0)in vec2 t;layout(location=0)uniform sampler2D i;layout(location=1)uniform vec3 c;void main(){float v=texture(i,t).r,E=fwidth(v)*.6;C=vec4(c,smoothstep(.5-E,.5+E,v));}"sv){
         create_buffer_2_2_4(vao,vbo);
     }
     void SDFRenderer::draw_stretched(const Texture<TextureType::TWO_D>& sdf,cppp::fvec2 pos,cppp::fvec2 size,cppp::fvec3 color) const{
